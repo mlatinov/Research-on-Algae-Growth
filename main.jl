bb9fbe01bd2d5d84d4c3eb7018be445ca016d0aa
mlatinov <latinovmetodi@gmail.com>
2026-05-02 01:42:59 +0300

    update the pipile with positional argumets and two models tested in simulation kinda doing what they are supposed to

--- main.jl
+++ main.jl
@@ -6,51 +6,45 @@ using Dagger
 include("functions/helpers.jl/general_helpers.jl")
 include("functions/eda_f/eda_f.jl")
 include("functions/modeling_f/dgps.jl")
+include("functions/modeling_f/bglm_models.jl")
 using .General_helpers
 using .EDA
 using .DGPs
-
+using .BGLM
 
 # Pipeline Tasks
 
 # Load the data 
-algae_data = Dagger.@spawn(General_helpers.load_algae(url = "data/algeas.csv", sample_size = 1000));
+algae_data = Dagger.@spawn(General_helpers.load_algae("data/algeas.csv", 1000));
 
 
 # Exploratory Analysis 
-algae_eda = Dagger.@spawn(EDA.eda_f(algae_data = algae_data));
- 
+algae_eda = Dagger.@spawn(EDA.eda_f(algae_data));
+
+# Data Generative Processs ==========================================================================================
+
 # DGP 1 for the Family of GLMs Baseline Quadratic Regression (Light Only)
-dgp_1 = Dagger.@spawn(DGPs.dgp_quadratic_baseline(n = 1000));
+sim_dgp_1 = Dagger.@spawn(DGPs.dgp_quadratic_baseline(1000));
 
 # DGP 2 for the Falily of GLMs  Full Additive GLM (Quadratic Light + Six Linear Predictors)
-dgp_2 = Dagger.@spawn(DGPs.dgp_full_quadratic(n = 1000));
+sim_dgp_2 = Dagger.@spawn(DGPs.dgp_full_quadratic(1000));
 
 # DGP 3 for the Family of GLMs Eilers Peeters PI Curve + Linear Secondary Predictors
-dgp_3 = Dagger.@spawn(DGPs.dgp_eilers_peeters(n = 1000));
+sim_dgp_3 = Dagger.@spawn(DGPs.dgp_eilers_peeters(1000));
 
 # DGP 4 for the Family of SEMs Two-Factor Reflective CFA with Quadratic Light Structural Equation
-dgp_4 = Dagger.@spawn(DGPs.dgp_sema(n = 1000));
+sim_dgp_4 = Dagger.@spawn(DGPs.dgp_sema(1000));
 
 # DGP 5 for the Family of SEMs  Two-Factor Reflective CFA with Eilers–Peeters Light Structural Equation
-dgp_5 = Dagger.@spawn(DGPs.dgp_semb(n =  1000));
+sim_dgp_5 = Dagger.@spawn(DGPs.dgp_semb(1000));
+
+# Model Specifications ==============================================================================================
+
+# BM-1 : Baseline Quadratic Regression (Light Only) Test in simulation against DGP 1 for Parametric Recovery 
+bqrl_sim = Dagger.@spawn(BGLM.bayes_quadratic_regreesion_light(sim_dgp_1, 1000));
+
+# BM-2: Bayesian Full Additive GLM (Quadratic Light + Six Linear Predictors)
+bfag_sim = Dagger.@spawn(BGLM.bayes_full_quadratic_regreesion(sim_dgp_2, 1000));
+
 
 # Run the Pipeline 
-data = fetch(algae_data)
-dgp_1_data = fetch(dgp_1)
-dgp_2_data = fetch(dgp_2)
-dgp_3_data = fetch(dgp_3)
-dgp_4_data = fetch(dgp_4)
-dgp_5_data = fetch(dgp_5)
-
-eda = fetch(algae_eda)
-using DataFrames
-using DataVoyager
-describe(data)
-describe(dgp_1_data)
-describe(dgp_2_data)
-describe(dgp_3_data)
-describe(dgp_4_data)
-describe(dgp_5_data)
-
-v1 = Voyager(dgp_5_data)
