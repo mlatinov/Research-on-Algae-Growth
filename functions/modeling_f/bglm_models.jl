8646dcb4c2f0f1847d378f0e8370d5c55496d6a2
mlatinov <latinovmetodi@gmail.com>
2026-05-02 23:41:46 +0300

    add BM-3: Eilers Peeters PI Curve + Linear Secondary Predictors

--- functions/modeling_f/bglm_models.jl
+++ functions/modeling_f/bglm_models.jl
@@ -101,9 +101,79 @@ function bayes_full_quadratic_regreesion(data, iters = 1000)
 
 end
 
+# BM-3: Eilers Peeters PI Curve + Linear Secondary Predictors
+export bepl_full
 
+function bepl_full(data, iters = 1000)
 
+    # Get the simulation data from the DGP
+    sim_data = data[:sim_data]
 
+    # Pre-compute the mean of Light outside the model 
+    Ī = mean(sim_data[!, :Light])
+
+    @model function bepl(Ιᵢ, Nᵢ, Feᵢ, Pᵢ, Tᵢ, Hᵢ, Cᵢ, Y, Ī)
+
+        # Biological EP parameters 
+        I_star ~ truncated(Normal(1000.0, 300.0), 100.0, 2500.0)
+        α₀     ~ truncated(Normal(0.0, 0.05),     0.0,   Inf)
+
+        # Recover raw EP parameters analytically 
+        γₑ = 1.0 / α₀
+        α  = γₑ / I_star^2
+        βₑ = 1.0/Pm - 2.0 * sqrt(α * γₑ)
+
+        # Guard unimodality: βₑ ≤ 0 means the curve has no finite peak.
+        if βₑ <= 0.0
+            Turing.@addlogprob! -Inf
+            return
+        end
+
+        # Normalised PI curve 
+        fPI_peak = I_star / (α * I_star^2 + βₑ * I_star + γₑ) 
+        fPIn = fPI ./ fPI_peak    
+
+        # Center the PI curve 
+        fPI_mean  = Ī  / (α * Ī^2  + βₑ * Ī  + γₑ)
+        fPIn_c    = fPIn .- (fPI_mean / Pm)
+        
+        # Structural priors 
+        δ₀ ~ Normal(3000.0, 500.0)
+        δ₁ ~ truncated(Normal(0.0, 2000.0), 0.0, Inf)
+        δ₂ ~ Normal(0.0, 50.0)   # Nitrate
+        δ₃ ~ Normal(0.0, 50.0)   # Iron
+        δ₄ ~ Normal(0.0, 50.0)   # Phosphate
+        δ₅ ~ Normal(0.0, 50.0)   # Temperature
+        δ₆ ~ Normal(0.0, 50.0)   # pH
+        δ₇ ~ Normal(0.0, 50.0)   # CO₂
+        σ  ~ truncated(Normal(0.0, 500.0), 0.0, Inf)
+
+        # Conditional mean 
+        μᵢ = δ₀ .+ δ₁ .* fPIn_c .+
+             δ₂ .* Nᵢ  .+ δ₃ .* Feᵢ .+ δ₄ .* Pᵢ .+
+             δ₅ .* Tᵢ  .+ δ₆ .* Hᵢ  .+ δ₇ .* Cᵢ
+
+        # Tobit likelihood
+        log_lik_censored  = logcdf.(Normal.(μᵢ, σ), 0.0)
+        log_lik_observed  = logpdf.(Normal.(μᵢ, σ), Y)
+        Turing.@addlogprob! sum(ifelse.(Y .<= 0.0, log_lik_censored, log_lik_observed))
+    end
+
+    model_bepl = bepl(
+        sim_data[!, :Light],
+        sim_data[!, :Nitrate],
+        sim_data[!, :Iron],
+        sim_data[!, :Phosphate],
+        sim_data[!, :Temperature],
+        sim_data[!, :pH],
+        sim_data[!, :CO2],
+        sim_data[!, :Population],
+        Ī
+    )
+
+    chain = sample(model_bepl, NUTS(0.65), MCMCSerial(), iters, 4)
+    return chain
+end
 
 
 
