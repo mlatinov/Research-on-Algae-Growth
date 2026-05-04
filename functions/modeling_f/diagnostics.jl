439bd950d9131b7fe956471bcbf77e99226375a4
mlatinov <latinovmetodi@gmail.com>
2026-05-04 23:54:07 +0300

    trying things ... i dont know

--- functions/modeling_f/diagnostics.jl
+++ functions/modeling_f/diagnostics.jl
@@ -6,19 +6,22 @@ using StatsPlots
 # Convergent Diagnostics 
 export conv_diagnostics
 
-function conv_diagnostics(chain)
+function conv_diagnostics(chain, parameters)
+
+    # Subset the chain by parameters 
+    chain_of_interest = chain[parameters]
 
     # Trace + density 
-    trace = plot(chain, size = (1200, 800))
+    trace = plot(chain_of_interest, size = (1200, 800))
 
     # R̂ and ESS  
     stats = summarystats(chain)
 
     # Autocorrelation 
-    autocor_plot = plot(autocorplot(chain), size = (1200, 800))
+    autocor_plot = plot(autocorplot(chain_of_interest), size = (1200, 800))
 
     # Corner plot joint posteriors 
-    corner_plot = corner(chain, size = (1400, 1400))
+    corner_plot = corner(chain_of_interest, size = (1400, 1400))
 
     return Dict(
         :trace    => trace,
