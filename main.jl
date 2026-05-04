ef82d3f383c0682f4b1c622212e25852305feb26
mlatinov <latinovmetodi@gmail.com>
2026-05-04 23:53:31 +0300

    trying things ... i dont know

--- main.jl
+++ main.jl
@@ -19,7 +19,7 @@ using .Diagnostics
 # Pipeline Tasks
 
 # Load the data 
-algae_data = Dagger.@spawn(General_helpers.load_algae("data/algeas.csv", 1000));
+algae_data = Dagger.@spawn(General_helpers.load_algae("data/algeas.csv", 3000));
 
 
 # Exploratory Analysis 
@@ -34,10 +34,10 @@ sim_dgp_1 = Dagger.@spawn(DGPs.dgp_quadratic_baseline(1000));
 sim_dgp_2 = Dagger.@spawn(DGPs.dgp_full_quadratic(1000));
 
 # DGP 3 for the Family of GLMs Eilers Peeters PI Curve + Linear Secondary Predictors
-sim_dgp_3 = Dagger.@spawn(DGPs.dgp_eilers_peeters(5000));
+sim_dgp_3 = Dagger.@spawn(DGPs.dgp_eilers_peeters(2000));
 
 # DGP 4 for the Family of SEMs Two-Factor Reflective CFA with Quadratic Light Structural Equation
-sim_dgp_4 = Dagger.@spawn(DGPs.dgp_sema(500));
+sim_dgp_4 = Dagger.@spawn(DGPs.dgp_sema(1000));
 
 # DGP 5 for the Family of SEMs  Two-Factor Reflective CFA with Eilers–Peeters Light Structural Equation
 sim_dgp_5 = Dagger.@spawn(DGPs.dgp_semb(1000));
@@ -51,10 +51,10 @@ bqrl_sim = Dagger.@spawn(BGLM.bayes_quadratic_regreesion_light(sim_dgp_1, 1000))
 bfag_sim = Dagger.@spawn(BGLM.bayes_full_quadratic_regreesion(sim_dgp_2, 1000));
 
 # BM-3: Eilers Peeters PI Curve + Linear Secondary Predictors for Parametric Recovery 
-bepl_sim = Dagger.@spawn(BGLM.bepl_full(sim_dgp_3, 500));
+bepl_sim = Dagger.@spawn(BGLM.bepl_full(sim_dgp_3, 2000));
 
 # BM-4: SEM-A: Two-Factor Reflective CFA with Quadratic Light Structural Equation for Parametric Recovery 
-bsema_sim = Dagger.@spawn(BSEM.sema_tfr_cfa_qlse(sim_dgp_4, 1000));
+bsema_sim = Dagger.@spawn(BSEM.sema_tfr_cfa_qlse(sim_dgp_4, 2000));
 
 # BM-5: SEM-B: Two-Factor Reflective CFA with Eilers–Peeters Light Structural Equation for Parametric Recovery 
 bsemb_sim = Dagger.@spawn(BSEM.semb_tfr_cfa_ep_light(sim_dgp_5, 1000));
@@ -67,12 +67,25 @@ bqrl_real = Dagger.@spawn(BGLM.bayes_quadratic_regreesion_light(algae_data, 1000
 # BM-2: Bayesian Full Additive GLM (Quadratic Light + Six Linear Predictors) Real data 
 bfag_real = Dagger.@spawn(BGLM.bayes_full_quadratic_regreesion(algae_data, 1000, false));
 
+# BM-3: Eilers Peeters PI Curve + Linear Secondary Predictors for Parametric Recovery 
+bepl_real = Dagger.@spawn(BGLM.bepl_full(algae_data, 1000, false));
+
 # Convergent Diagnostics ==============================================================================================
-bqrl_real_conv = Dagger.@spawn(Diagnostics.conv_diagnostics(bqrl_real));
-bfag_real_conv = Dagger.@spawn(Diagnostics.conv_diagnostics(bfag_real));
+bqrl_real_conv = Dagger.@spawn(Diagnostics.conv_diagnostics(bqrl_real, [:β₀,:β₁,:β₂,:σ])); # Fine 
+bfag_real_conv = Dagger.@spawn(Diagnostics.conv_diagnostics(bfag_real, [:β₀,:β₁,:β₂,:σ])); # Fine 
+bepl_real_conv = Dagger.@spawn(Diagnostics.conv_diagnostics(bepl_real, [:I_star,:Pm,:α₀,:σ])); # Fuck off
 
 
 
+check_model = fetch(bepl_sim_fixed)
+describe(check_model)
+truth = fetch(sim_dgp_3)
+a = truth[:ground_truth]
+
+
+example = fetch(bepl_real_conv)
+conv_example = fetch(bepl_real_conv)
+conv_example[:autocor]
 
 # Posterior Predictive Checks =========================================================================================
 
